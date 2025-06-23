40251e897ea52d0431761eafe55a3f821f6f917b
danielchinchilla <danielchsa07@gmail.com>
2025-06-22 18:07:28 -0600

    Update Navbar.tsx

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,45 +1,51 @@
-import { Spacer, Text, useTheme, Link } from "@nextui-org/react";
-import Image from "next/image";
-import NextLink from "next/link";
+import { Spacer, Text, useTheme, Link } from '@nextui-org/react'
+import Image from 'next/image'
+import NextLink from 'next/link'
 
 export const Navbar = () => {
-  const { theme } = useTheme();
+  const { theme } = useTheme()
   return (
     <div
       style={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        background: theme?.colors.gray900.value,
-        padding: "0px 20px",
-      }}
-    >
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        background: theme?.colors.purple900.value,
+        padding: '0px 20px',
+      }}>
       <Image
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        alt='Pokemon image'
+        src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
         width={70}
         height={70}
       />
-      <NextLink href={"/"}>
+      <NextLink href={'/'}>
         <Link>
-          <Text color="white" h2>
+          <Text
+            color='white'
+            h2>
             P
           </Text>
-          <Text color="white" h3>
+          <Text
+            color='white'
+            h3>
             okemon!
           </Text>
         </Link>
       </NextLink>
 
       <Spacer css={{ flex: 1 }} />
-      <NextLink href={"/favorites"}>
+      <NextLink href={'/favorites'}>
         <Link>
-          <Text color="white" h4>
+          <Text
+            color='white'
+            h4>
             Favoritos
           </Text>
         </Link>
       </NextLink>
     </div>
-  );
-};
+  )
+}
